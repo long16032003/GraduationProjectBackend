88488866a2c0d013ca437ffeb8d85a2f00a505f2
Dinh Bang <ndinhbang111@gmail.com>
2025-05-03 21:15:53 +0700

    use Redis to manage rate limiting

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,16 +6,21 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Foundation\Http\Middleware\TrimStrings;
 use Illuminate\Http\Middleware\HandleCors;
+
 //use Illuminate\Cookie\Middleware\EncryptCookies;
 //use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // https://laravel.com/docs/12.x/routing#throttling-with-redis
+        // https://laravel.com/docs/12.x/rate-limiting#cache-configuration
+        $middleware->throttleWithRedis();
+
         $middleware
             // we will handle cors in .htaccess file or apache config, so we dont need this middleware
             ->remove(HandleCors::class)
